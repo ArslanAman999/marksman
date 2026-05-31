151ffc09445eeaa38def0b6064e25ffcd1a095b9
ArslanAman999 <arslanaman999@gmail.com>
2026-05-31 16:05:13 +0500

    update Shoes model: add id, stockQuantity fields and fromJson method

--- lib/Shoes.dart
+++ lib/Shoes.dart
@@ -1,13 +1,31 @@
 class Shoes {
+  final int id;
   final String name;
   final int price;
   final String imagelocation;
   final String description;
+  final int stockQuantity;
 
   Shoes({
+    required this.id,
     required this.name,
     required this.price,
     required this.imagelocation,
     required this.description,
+    required this.stockQuantity,
   });
+
+  // Converts the JSON response from API into a Shoes object
+  // API sends: {"id":1,"name":"Charm Walk Beige","price":200,...}
+  // This method maps each JSON field to the correct class property
+  factory Shoes.fromJson(Map<String, dynamic> json) {
+    return Shoes(
+      id:            json['id'],
+      name:          json['name'],
+      price:         json['price'],
+      imagelocation: 'assets/images/${json['image']}',
+      description:   json['description'],
+      stockQuantity: json['stock_quantity'],
+    );
+  }
 }
